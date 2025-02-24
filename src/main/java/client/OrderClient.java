cfc3b389e804a30affc2bd083e7a69e55a7af17c
Shalgenbaev <t970399@yandex.ru>
2025-02-24 14:24:56 +0500

    For verification

--- src/main/java/client/OrderClient.java
+++ src/main/java/client/OrderClient.java
@@ -1,4 +1,14 @@
 package client;
 
-public class OrderClient {
+import dto.OrderCreateRequest;
+import io.restassured.response.Response;
+
+public class OrderClient extends RestClient {
+    public Response createOrder(OrderCreateRequest orderCreateRequest) {
+        return getDefaultRequestSpecification()
+                .body(orderCreateRequest)
+                .when()
+                .post("/orders");
+
+    }
 }
